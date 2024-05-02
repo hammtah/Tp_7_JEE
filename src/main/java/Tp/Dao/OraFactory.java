2513e14727cf83543af7d72f27faa73c0f94e5e7
hammtah <115116715+hammtah@users.noreply.github.com>
2024-05-02 09:16:42 +0100

    first commit

--- src/main/java/Tp/Dao/OraFactory.java
+++ src/main/java/Tp/Dao/OraFactory.java
@@ -1,23 +1,31 @@
 package Tp.Dao;
-
+import Tp.Dao.interfaces.*;
+import Tp.Dao.implementation.*;
 import java.sql.DriverManager;
 import java.sql.*;
 
+//Cette class est reponsable à l'etablissement de connection, ainsi la creation des differents 'objets DAO'. 
+//Ces objets DAO retournés sont les implementations des interfaces situé dans Tp.Dao.interfaces.  
+//Ces interfaces contiennet les definitions des methodes de manipulation des <tableaux> de notre BD. 
+//donc ça permet l'isolation, puisque les methodes de manipulation des tables sont 
+//independants de l'implementation .
+//Cette methodologie devient envore plus utile quand vous travailler avec plusieurs SGBD   
 public class OraFactory {
 	public static final String driver="oracle.jdbc.driver.OracleDriver";
-	public static final String dbUrl="jdbc:oracle:thin:@//localhost:1521:xepdb1";
-	Connection conn = null;
+	public static final String dbUrl="jdbc:oracle:thin:testpractice/Test123@//localhost:1521/xepdb1";
 
-public Connection getConnection() {
+
+public static Connection getConnection() throws SQLException{
 	try {
 		Class.forName(driver);
 	}catch(ClassNotFoundException ec) {}
 	
-	try {
-		conn =  DriverManager.getConnection(dbUrl, "system", "clarence");
-	}catch(SQLException se) {}
-	
-	return conn;
+	return DriverManager.getConnection(dbUrl);
+
+}
+
+public static UserDao getUserDao() {
+	return new UserDaoImpl();
 }
 
 }
--- src/main/java/Tp/beans/User.java
+++ src/main/java/Tp/beans/User.java
@@ -4,15 +4,15 @@ public class User {
 	private String nom;
 	private String prenom;
 	private String login;
-	
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
 	private String pass;
 	
+	public User() {
+		this.nom = null;
+		this.prenom = null;
+		this.login = null;
+		this.pass = null;
+	}
+
 	public String getNom() {
 		return nom;
 	}
@@ -30,5 +30,11 @@ public class User {
 	}
 	public void setPass(String pass) {
 		this.pass = pass;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
 	}
 }
